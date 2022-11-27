23e59622a22e46e14ee890f0ce8e096b54271780
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-11-28 08:46:54 +0900

    Update Picking-Numbers.cpp

--- Picking-Numbers.cpp
+++ Picking-Numbers.cpp
@@ -1,3 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
 int pickingNumbers(vector<int> a)
 {
     int result = 0;
